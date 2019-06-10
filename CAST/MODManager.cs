c96f2e53b7b5963e4332f05b7cb983dc5d8b9a71
riku1227 <rikum20021227@gmail.com>
2019-06-11 01:54:17 +0900

    🎉 Recreate CAST!

---
+++ CAST/MODManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CAST
+{
+    public class MODManager : MonoBehaviour
+    {
+
+        String temp = "";
+
+        //SceneFirstDownload#Start() で一番最初に呼び出される
+        public static void InitMODManager()
+        {
+            var go = new GameObject();
+            DontDestroyOnLoad(go);
+            go.AddComponent<MODManager>();
+        }
+
+        void Start()
+        {
+            SceneManager.sceneLoaded += onSceneLoaded;
+        }
+
+        void OnGUI()
+        {
+            GUI.TextArea(new Rect(10, 10, 300, 100), temp);
+        }
+
+        void onSceneLoaded(Scene nextScene, LoadSceneMode mode)
+        {
+            temp += nextScene.name + ":" + nextScene.isLoaded;
+            if(nextScene.name.IndexOf("Main") != -1)
+            {
+                //カメラのy軸制限を解除
+                GameMain.Instance.MainCamera.GetComponent<UltimateOrbitCamera>().limitY = false;
+            }
+        }
+    }
+}
--- CAST/Properties/AssemblyInfo.cs
+++ CAST/Properties/AssemblyInfo.cs
@@ -3,14 +3,14 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
-// アセンブリに関連付けられている情報を変更するには、
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("CAST")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CAST")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,17 +20,17 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("f576b4bc-ec5f-4770-967f-af368d672b9d")]
+[assembly: Guid("abfef490-f6c0-4ed8-974a-6c137a2570fe")]
 
-// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
 //
 //      メジャー バージョン
 //      マイナー バージョン
 //      ビルド番号
-//      Revision
+//      リビジョン
 //
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
-// 以下のように '*' を使用します:
+// 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
