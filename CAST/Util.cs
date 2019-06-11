0a4efc53ab4791c28fb62a7944daa02453a5f4ac
riku1227 <rikum20021227@gmail.com>
2019-06-11 22:09:07 +0900

    ➕ Utilにプライベートフィールド操作メソッドを追加

--- CAST/Util.cs
+++ CAST/Util.cs
@@ -16,5 +16,17 @@ namespace CAST
             FieldInfo info = type.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
             info.SetValue(null, value);
         }
+
+        public static object getPrivateField(Type type, object instance, String fieldName)
+        {
+            FieldInfo info = type.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+            return info.GetValue(instance);
+        }
+
+        public static void setPrivateField(Type type, object instance, String fieldName, object value)
+        {
+            FieldInfo info = type.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+            info.SetValue(instance, value);
+        }
     }
 }
