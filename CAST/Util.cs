116d3e47d5ae862397f04b5ac412640f170dc739
riku1227 <rikum20021227@gmail.com>
2019-06-11 18:25:20 +0900

    ➕ Utilクラスを追加

---
+++ CAST/Util.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace CAST
+{
+    class Util
+    {
+        public static object getPrivateStaticField(Type type, String fieldName)
+        {
+            FieldInfo info = type.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
+            return info.GetValue(null);
+        }
+
+        public static void setPrivateStaticField(Type type, String fieldName, object value)
+        {
+            FieldInfo info = type.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
+            info.SetValue(null, value);
+        }
+    }
+}
