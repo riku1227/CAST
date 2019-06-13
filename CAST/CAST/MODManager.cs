da5627b4ff13e2b7fb4e0d69fc107ee4bdb3327d
riku1227 <rikum20021227@gmail.com>
2019-06-13 21:47:04 +0900

    🎨 不要なコードを削除

--- CAST/CAST/MODManager.cs
+++ CAST/CAST/MODManager.cs
@@ -1,29 +1,16 @@
-﻿using Harmony;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text;
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 
 namespace CAST
 {
     public class MODManager : MonoBehaviour
     {
-        static String temp = "";
         static FileSystemStorage fileSystem;
 
         //SceneFirstDownload#Start() で一番最初に呼び出される
         public static void InitMODManager()
         {
-            var harmony = HarmonyInstance.Create("com.riku1227.cast");
-            //harmony.PatchAll(Assembly.GetExecutingAssembly());
             UI.LogUI.Init();
             var go = new GameObject();
             DontDestroyOnLoad(go);
@@ -49,24 +36,11 @@ namespace CAST
 
         void onSceneLoaded(Scene nextScene, LoadSceneMode mode)
         {
-            temp += nextScene.name + ":" + nextScene.isLoaded;
             if(nextScene.name.IndexOf("Main") != -1)
             {
                 //カメラのy軸制限を解除
                 GameMain.Instance.MainCamera.GetComponent<UltimateOrbitCamera>().limitY = false;
-
-                //uGUISystemDialog.OpenYesNo("TestDialog", delegate() { }, delegate () { });
             }
-        }
-    }
-
-    [HarmonyPatch (typeof(String))]
-    [HarmonyPatch ("ToUpper")]
-    public class Patch
-    {
-        static void Prefix()
-        {
-            Debug.Log("ToUpper");
         }
     }
 }
