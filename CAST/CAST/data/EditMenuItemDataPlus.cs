61b4766c30043c4d48fcb7197df72dd2b9245372
riku1227 <rikum20021227@gmail.com>
2019-06-18 21:03:01 +0900

    ➕ EditMenuItemDataPlusを追加

---
+++ CAST/CAST/data/EditMenuItemDataPlus.cs
@@ -0,0 +1,16 @@
+﻿using SCENE_EDIT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAST.data
+{
+    public class EditMenuItemDataPlus
+    {
+        public EditMenuItemData editMenuItemData;
+        public String zurashiMenuFileName;
+        public String mekureMenuFileName;
+        public String mekureBackMenuFileName;
+    }
+}
